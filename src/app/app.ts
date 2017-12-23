be15e7b6418ab8832cc2eda635f1c3fabb22cfe4
15138730370 <1037483576@qq.com>
2017-12-23 13:24:45 +0800

    save to back

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,8 @@ export { CoreToastComponent } from './core-toast/core-toast';
 export { CoreLoadingComponent } from './core-loading/core-loading';
 export { CoreRootComponent } from './core-root/core-root';
 export { CoreMenuComponent } from './core-menu/core-menu';
+export { CorePopoverComponent, CorePopoverWidget } from './core-popover/core-popover';
+
 export { MeepoCoreModule } from './core.module';
 export { MeepoCoreServiceModule } from './core-service.module';
 
