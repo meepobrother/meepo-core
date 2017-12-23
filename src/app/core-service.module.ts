18e4dde0a52537af71ed61697f13e8597bccc1ea
15138730370 <1037483576@qq.com>
2017-12-23 09:46:02 +0800

    save to back

--- src/app/core-service.module.ts
+++ src/app/core-service.module.ts
@@ -1,47 +1,13 @@
 import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { CoreService } from './core.service';
-
-import { CoreAlertComponent } from './core-alert/core-alert';
-import { CoreConfirmComponent } from './core-confirm/core-confirm';
-import { CoreToastComponent } from './core-toast/core-toast';
-import { CoreLoadingComponent } from './core-loading/core-loading';
-import { CoreRootComponent } from './core-root/core-root';
-import { CoreMenuComponent } from './core-menu/core-menu';
-
-const CoreComponents: any[] = [
-    CoreAlertComponent,
-    CoreRootComponent,
-    CoreConfirmComponent,
-    CoreToastComponent,
-    CoreLoadingComponent,
-    CoreMenuComponent
-];
-
-export function coreFactory() {
-    return new CoreService();
-}
-
 @NgModule({})
 export class MeepoCoreServiceModule {
-
-    constructor( @Optional() @SkipSelf() parentModule: MeepoCoreServiceModule) {
-        if (parentModule) {
-            throw new Error(
-                'CoreModule is already loaded. Import it in the AppModule only');
-        }
-    }
-
     public static forRoot(): ModuleWithProviders {
         return {
             ngModule: MeepoCoreServiceModule,
             providers: [
-                {
-                    provide: CoreService,
-                    useFactory: coreFactory
-                }
-            ],
+                CoreService
+            ]
         };
     }
 }
