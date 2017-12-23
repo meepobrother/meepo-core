e1231a3baaa5c08787717125d578f47a939a11c7
15138730370 <1037483576@qq.com>
2017-12-23 17:03:12 +0800

    save to back

--- src/app/core-service.module.ts
+++ src/app/core-service.module.ts
@@ -1,16 +1,11 @@
 import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { getCoreService, CoreService } from './core.service';
-@NgModule({})
-export class MeepoCoreServiceModule {
-    public static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: MeepoCoreServiceModule,
-            providers: [
-                {
-                    provide: CoreService,
-                    useFactory: getCoreService
-                }
-            ]
-        };
-    }
-}
+@NgModule({
+    providers: [
+        {
+            provide: CoreService,
+            useFactory: getCoreService
+        }
+    ]
+})
+export class MeepoCoreServiceModule {}
