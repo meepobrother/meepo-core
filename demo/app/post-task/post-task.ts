6b3d3c9e52a33ab82e4e264a9192f0ae62cfde9e
15138730370 <1037483576@qq.com>
2017-12-23 12:36:12 +0800

    save to back

--- demo/app/post-task/post-task.ts
+++ demo/app/post-task/post-task.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { CoreService } from '../../../src/app/app';
 @Component({
     selector: 'post-task',
     templateUrl: 'post-task.html'
@@ -7,7 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class PostTaskComponent implements OnInit {
     @Input() data: any;
-    constructor() { }
+    constructor(
+        public core: CoreService
+    ) { }
 
     ngOnInit() { }
+
+    cancel() { 
+        this.core.closePopover();
+    }
+
+    pay() { }
 }
