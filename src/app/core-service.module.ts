19c01e3e6930a987084f7a0070ed741f58435426
15138730370 <1037483576@qq.com>
2017-12-29 06:02:40 +0800

    save to back

--- src/app/core-service.module.ts
+++ src/app/core-service.module.ts
@@ -1,13 +1,18 @@
 import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { getCoreService, CoreService } from './core.service';
 import { UtilService } from './util.service';
+import { DateTimeService } from './date-time';
+import { DomService } from './dom';
+
 @NgModule({
     providers: [
         {
             provide: CoreService,
             useFactory: getCoreService
         },
-        UtilService
+        UtilService,
+        DateTimeService,
+        DomService
     ]
 })
 export class MeepoCoreServiceModule {}
