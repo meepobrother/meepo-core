d732f954e8f670aa79a14c56c88779a5674573ef
15138730370 <1037483576@qq.com>
2017-12-29 06:16:31 +0800

    save to back

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,3 +11,7 @@ export { MeepoCoreModule } from './core.module';
 export { MeepoCoreServiceModule } from './core-service.module';
 
 export { CoreService } from './core.service';
+export { DateTimeService } from './date-time';
+export { DomService } from './dom';
+export { UtilService } from './util.service';
+export { QueryParams } from './query-params';
--- src/app/dom.ts
+++ src/app/dom.ts
@@ -6,8 +6,6 @@ export class DomService {
     constructor() { }
 
     pointerCoord(ev: any): PointerCoordinates {
-        // get coordinates for either a mouse click
-        // or a touch depending on the given event
         if (ev) {
             var changedTouches = ev.changedTouches;
             if (changedTouches && changedTouches.length > 0) {
@@ -31,7 +29,64 @@ export class DomService {
         }
         return false;
     }
+
+    isTextInput(ele: any) {
+        return !!ele &&
+            (ele.tagName === 'TEXTAREA' ||
+                ele.contentEditable === 'true' ||
+                (ele.tagName === 'INPUT' && !(NON_TEXT_INPUT_REGEX.test(ele.type))));
+    }
+
+    copyInputAttributes(srcElement: HTMLElement, destElement: HTMLElement) {
+        const attrs = srcElement.attributes;
+        for (var i = 0; i < attrs.length; i++) {
+            var attr = attrs[i];
+            if (SKIP_INPUT_ATTR.indexOf(attr.name) === -1) {
+                destElement.setAttribute(attr.name, attr.value);
+            }
+        }
+    }
+
+
+    getCss(docEle: HTMLElement) {
+        const css: {
+            transform?: string;
+            transition?: string;
+            transitionDuration?: string;
+            transitionDelay?: string;
+            transitionTimingFn?: string;
+            transitionStart?: string;
+            transitionEnd?: string;
+            transformOrigin?: string;
+            animationDelay?: string;
+        } = {};
+        var i: number;
+        var keys = ['webkitTransform', '-webkit-transform', 'webkit-transform', 'transform'];
+        for (i = 0; i < keys.length; i++) {
+            if ((<any>docEle.style)[keys[i]] !== undefined) {
+                css.transform = keys[i];
+                break;
+            }
+        }
+        keys = ['webkitTransition', 'transition'];
+        for (i = 0; i < keys.length; i++) {
+            if ((<any>docEle.style)[keys[i]] !== undefined) {
+                css.transition = keys[i];
+                break;
+            }
+        }
+        var isWebkit = css.transition.indexOf('webkit') > -1;
+        css.transitionDuration = (isWebkit ? '-webkit-' : '') + 'transition-duration';
+        css.transitionTimingFn = (isWebkit ? '-webkit-' : '') + 'transition-timing-function';
+        css.transitionDelay = (isWebkit ? '-webkit-' : '') + 'transition-delay';
+        css.transitionEnd = (isWebkit ? 'webkitTransitionEnd ' : '') + 'transitionend';
+        css.transformOrigin = (isWebkit ? '-webkit-' : '') + 'transform-origin';
+        css.animationDelay = (isWebkit ? 'webkitAnimationDelay' : 'animationDelay');
+        return css;
+    }
 }
+const SKIP_INPUT_ATTR = ['value', 'checked', 'disabled', 'readonly', 'placeholder', 'type', 'class', 'style', 'id', 'autofocus', 'autocomplete', 'autocorrect'];
+export const NON_TEXT_INPUT_REGEX = /^(radio|checkbox|range|file|submit|reset|color|image|button)$/i;
 
 export interface PointerCoordinates {
     x?: number;
