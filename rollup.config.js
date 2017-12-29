deecb8aa535698e3992e83b4067eb08d6f0ba924
15138730370 <1037483576@qq.com>
2017-12-29 12:33:15 +0800

    save to back

--- rollup.config.js
+++ rollup.config.js
@@ -231,7 +231,8 @@ let globals = {
     'rxjs/operators/throttleTime': 'Rx.Observable.prototype',
     'rxjs/operators/tap': 'Rx.Observable.prototype',
     'meepo-xscroll': 'meepo-xscroll',
-    'meepo-popover': 'meepo-popover'
+    'meepo-popover': 'meepo-popover',
+    'meepo-uuid': 'meepo-uuid'
 };
 
 let plugins = [
